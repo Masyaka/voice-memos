232695641cb4340bbbd58e23650a7af368ef0f5b
jbarlukov <maestroz89@gmail.com>
2018-06-08 00:09:43 +0800

    Added appbar to layout. Defined default font.

--- src/bundles/common/components/Layout.js
+++ src/bundles/common/components/Layout.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 export default class MainPage extends React.Component{
   render(){
-    return <div>{this.props.children}</div>;
+    return(
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar>
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Voice memos
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div style={{marginTop: 64}}>{this.props.children}</div>
+      </React.Fragment>
+    );
   }
 }
